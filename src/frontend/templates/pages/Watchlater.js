e541edb81d35edc8b04924d33dc8f41aee8c91c2
Harshit <najay357@gmail.com>
2022-04-08 17:49:04 +0530

    changed small logic in watch later

--- src/frontend/templates/pages/Watchlater.js
+++ src/frontend/templates/pages/Watchlater.js
@@ -10,7 +10,7 @@ export const Watchlater = () => {
         <main className='main'>
             {userState.foundUser && userState.foundUser.watchlater.length > 0 ? (<>
                 <div className='videos-wrapper px-xs py-sm d-flex fw-wrap ai-c jc-c gap-1 mx-auto'>
-                    {[...userState.foundUser.watchlater].map(video => <Video fromHistory={false} fromPlaylists={false} key={video._id} videoDetails={video} />)}
+                    {[...userState.foundUser.watchlater].map(video => <Video fromHistory={false} fromPlaylists={'none'} key={video._id} videoDetails={video} />)}
                 </div>
             </>) : (<p className='f-2x f-w-500 px-sm py-sm ta-c'> No Videos in Watch later</p>)}
         </main>
